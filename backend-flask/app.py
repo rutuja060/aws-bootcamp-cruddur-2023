a52caefe35cdfc1cbdd7886bbc3763b44a5cc229
Rutuja Kadam <kadamrutuja103@gmail.com>
2023-03-02 17:59:53 +0000

    implement cloudwatch logs

--- backend-flask/app.py
+++ backend-flask/app.py
@@ -32,13 +32,13 @@ import logging
 from time import strftime
 
 # Configuring Logger to Use CloudWatch
-LOGGER = logging.getLogger(__name__)
-LOGGER.setLevel(logging.DEBUG)
-console_handler = logging.StreamHandler()
-cw_handler = watchtower.CloudWatchLogHandler(log_group='cruddur')
-LOGGER.addHandler(console_handler)
-LOGGER.addHandler(cw_handler)
-LOGGER.info("test log")
+#LOGGER = logging.getLogger(__name__)
+#LOGGER.setLevel(logging.DEBUG)
+#console_handler = logging.StreamHandler()
+#cw_handler = watchtower.CloudWatchLogHandler(log_group='cruddur')
+#LOGGER.addHandler(console_handler)
+#LOGGER.addHandler(cw_handler)
+#LOGGER.info("test log")
 
 #HONEYCOMB
 # Initialize tracing and an exporter that can send data to Honeycomb
@@ -50,14 +50,17 @@ tracer = trace.get_tracer(__name__)
 
 
 #XRAY
-xray_url = os.getenv("AWS_XRAY_URL")
-xray_recorder.configure(service='backend-flask', dynamic_naming=xray_url)
+#xray_url = os.getenv("AWS_XRAY_URL")
+#xray_recorder.configure(service='backend-flask', dynamic_naming=xray_url)
 
 
 
 app = Flask(__name__)
-XRayMiddleware(app, xray_recorder)
 
+#XRAY
+#XRayMiddleware(app, xray_recorder)
+
+#HONEYCOMB
 # Initialize automatic instrumentation with Flask
 FlaskInstrumentor().instrument_app(app)
 RequestsInstrumentor().instrument()
@@ -73,11 +76,11 @@ cors = CORS(
   methods="OPTIONS,GET,HEAD,POST"
 )
 
-@app.after_request
-def after_request(response):
-    timestamp = strftime('[%Y-%b-%d %H:%M]')
-    LOGGER.error('%s %s %s %s %s %s', timestamp, request.remote_addr, request.method, request.scheme, request.full_path, response.status)
-    return response
+#@app.after_request
+#def after_request(response):
+ #   timestamp = strftime('[%Y-%b-%d %H:%M]')
+  #  LOGGER.error('%s %s %s %s %s %s', timestamp, request.remote_addr, request.method, request.scheme, request.full_path, response.status)
+  #  return response
 
 
 @app.route("/api/message_groups", methods=['GET'])
--- backend-flask/services/home_activities.py
+++ backend-flask/services/home_activities.py
@@ -3,9 +3,10 @@ from opentelemetry import trace
 
 tracer = trace.get_tracer("home.activities")
 
+
 class HomeActivities:
   def run():
-    LOGGER.info("HomeActivities")
+    #logger.info("HomeActivities")
     with tracer.start_as_current_span("home-activities-mock-data"):
       span = trace.get_current_span()
       now = datetime.now(timezone.utc).astimezone()
